a8979204f84d38338a13b21bfca3f43fb9f4fa4d
Nikhil V <nikhil20165520@gmail.com>
2021-07-15 11:20:53 +0530

    small changes to primary key changed

--- app/src/main/java/com/narendra/timetable/Database/TimeTableContract.java
+++ app/src/main/java/com/narendra/timetable/Database/TimeTableContract.java
@@ -2,7 +2,7 @@ package com.narendra.timetable.Database;
 
 public class TimeTableContract {
     static final String tableName="TIMETABLE";
-    static final String timeTableId="rowid";
+    static final String timeTableId="_ID";
     static final String numberOfRows="NUMBEROFROWS";
     static final String numberOfperiods="NUMBEROFPERIODAPERDAY";
     static final String timeTableName="TIMETABLENAME";

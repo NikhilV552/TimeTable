7ea0423abc10074aec4fbd95ae0f6919531c2563
Narendra <narendra27001@gmail.com>
2021-07-16 20:41:42 +0530

    Created Hamburger Icon,drawer layout,navigational view and menu list and added hamburger functionality

--- app/src/main/java/com/narendra/timetable/Activity/MainActivity.java
+++ app/src/main/java/com/narendra/timetable/Activity/MainActivity.java
@@ -1,15 +1,19 @@
 package com.narendra.timetable.Activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.narendra.timetable.Adapter.DayAdapter;
-import com.narendra.timetable.Adapter.PeriodAdapter;
+import com.google.android.material.navigation.NavigationView;
 import com.narendra.timetable.Database.DatabaseTimeTableHelper;
 import com.narendra.timetable.Model.PeriodTimeModel;
 import com.narendra.timetable.Model.RowModel;
@@ -22,59 +26,101 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerPeriod;
-    RecyclerView recyclerDay;
+
+    DrawerLayout drawerLayout;
+    CoordinatorLayout coordinatorLayout;
+    Toolbar toolbar;
+    FrameLayout frameLayout;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        valueGV=findViewById(R.id.gridView);
-//        ArrayList<> contentArrayList=new ArrayList<Content>();
-//        contentArrayList.add(new Content("SSC"));
-//        contentArrayList.add(new Content("CGV"));
-//        contentArrayList.add(new Content("WT"));
-//        contentArrayList.add(new Content("AJAVA"));
-//        contentArrayList.add(new Content("Maths"));
-//        contentArrayList.add(new Content("MAD"));
-//        contentArrayList.add(new Content("CGVL"));
-//        contentArrayList.add(new Content("SSOSL"));
-//        contentArrayList.add(new Content("MBS"));
-//        contentArrayList.add(new Content("KJB"));
-//        contentArrayList.add(new Content("SHR"));
-//        contentArrayList.add(new Content("Netravathy"));
-//        contentArrayList.add(new Content("KMR"));
-//        contentArrayList.add(new Content("Sruthiba"));
-//        contentArrayList.add(new Content("Sruthiba"));
-//        contentArrayList.add(new Content("Nethravathy"));
-//        TimeTableAdapter adapter=new TimeTableAdapter(this,contentArrayList);
-//        valueGV.setAdapter(adapter);
-            recyclerPeriod=findViewById(R.id.recyclerPeriod);
-            recyclerDay=findViewById(R.id.recyclerDay);
 
-         DatabaseTimeTableHelper timeTableHelper=new DatabaseTimeTableHelper(this);
-//
-        TimeTableModel model1 = GenerateModelData.generateData("TIMETABLE_2",2,8);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+        toolbar = findViewById(R.id.toolbar);
+        frameLayout = findViewById(R.id.frameLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Timetable");
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,toolbar,R.string.navigation_open_drawer,R.string.navigation_close_drawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.item1:
+                    Toast.makeText(MainActivity.this, "Clicked on item1", Toast.LENGTH_LONG).show();break;
+                case R.id.item2:
+                    Toast.makeText(MainActivity.this, "Clicked on item2", Toast.LENGTH_LONG).show();break;
+                case R.id.item3:
+                    Toast.makeText(MainActivity.this, "Clicked on item3", Toast.LENGTH_LONG).show();break;
+                case R.id.item4:
+                    Toast.makeText(MainActivity.this, "Clicked on item4", Toast.LENGTH_LONG).show();break;
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
+
+        DatabaseTimeTableHelper timeTableHelper = new DatabaseTimeTableHelper(MainActivity.this);
+        TimeTableModel model1 = GenerateModelData.generateData("TIMETABLE_2", 2, 8);
         timeTableHelper.createTable(model1);
         System.out.println(model1);
-        int temp=timeTableHelper.getTimeTableId2("TIMETABLE_2");
-        Toast.makeText(this, "timetableid="+temp, Toast.LENGTH_LONG).show();
-        PeriodTimeModel initial=new PeriodTimeModel(new Time(00000000),new Time(00000000));
-        ArrayList<PeriodTimeModel> period=model1.getPeriodTimes();
-        HashMap<String,ArrayList<RowModel>> timeTableValues=model1.getTimeTableValues();
-        period.add(0,initial);
-//        System.out.println(period.size());
-        GridLayoutManager periodLayoutManager=new GridLayoutManager(this,period.size());
-        PeriodAdapter periodAdapter=new PeriodAdapter(this,period);
-        recyclerPeriod.setLayoutManager(periodLayoutManager);
-        recyclerPeriod.setAdapter(periodAdapter);
-//        for(String i: model1.getDays()){
-//            System.out.println(i);
-//        }
-        LinearLayoutManager dayLayoutManager=new LinearLayoutManager(this);
-        DayAdapter dayAdapter=new DayAdapter(this,model1.getDays(),timeTableValues,model1.getRowNames());
-        recyclerDay.setLayoutManager(dayLayoutManager);
-        recyclerDay.setAdapter(dayAdapter);
+        int temp = timeTableHelper.getTimeTableId2("TIMETABLE_2");
+        Toast.makeText(MainActivity.this, "timetableid=" + temp, Toast.LENGTH_LONG).show();
+        PeriodTimeModel initial = new PeriodTimeModel(new Time(00000000), new Time(00000000));
+        ArrayList<PeriodTimeModel> period = model1.getPeriodTimes();
+        HashMap<String, ArrayList<RowModel>> timeTableValues = model1.getTimeTableValues();
+        period.add(0, initial);
+
+//
+//        GridLayoutManager periodLayoutManager=new GridLayoutManager(this,period.size());
+//        PeriodAdapter periodAdapter=new PeriodAdapter(this,period);
+//        recyclerPeriod.setLayoutManager(periodLayoutManager);
+//        recyclerPeriod.setAdapter(periodAdapter);
+
+//
+//        LinearLayoutManager dayLayoutManager=new LinearLayoutManager(this);
+//        DayAdapter dayAdapter=new DayAdapter(this,model1.getDays(),timeTableValues,model1.getRowNames());
+//        recyclerDay.setLayoutManager(dayLayoutManager);
+//        recyclerDay.setAdapter(dayAdapter);
 
 
+        ArrayList<String> timeTable = new ArrayList<String>();
+        timeTable.add("TimeTable_1");
+        timeTable.add("TimeTable_2");
+        timeTable.add("TimeTable_3");
+        timeTable.add("TimeTable_4");
+        timeTable.add("TimeTable_5");
+        timeTable.add("TimeTable_6");
+        timeTable.add("TimeTable_7");
+        timeTable.add("TimeTable_8");
+        timeTable.add("TimeTable_9");
+        timeTable.add("TimeTable_10");
+        timeTable.add("TimeTable_11");
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.home)
+            drawerLayout.openDrawer(GravityCompat.START);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }

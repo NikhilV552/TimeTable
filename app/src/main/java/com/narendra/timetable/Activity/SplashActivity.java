e475f6ae54a7358a46565917b03460c457cd2c96
Nikhil V <nikhil20165520@gmail.com>
2021-07-18 12:33:33 +0530

    Splash activity condition condition added

--- app/src/main/java/com/narendra/timetable/Activity/SplashActivity.java
+++ app/src/main/java/com/narendra/timetable/Activity/SplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.narendra.timetable.Database.DatabaseTimeTableHelper;
 import com.narendra.timetable.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,18 +14,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DatabaseTimeTableHelper helper=new DatabaseTimeTableHelper(this);
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                                /*
-                if(database!=null)
+                int len=helper.getAllTimeTableNamesPresent().size();
+                if(len!=0)
                     startActivity(new Intent(SplashActivity.this,MainActivity.class));
                  else
                     startActivity(new Intent(SplashActivity.this,CreateActivity.class));
-                 */
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
+
+                /*Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);*/
                 finish();
             }
         },3000);
